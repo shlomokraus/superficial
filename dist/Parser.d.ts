df3ac71425d81a877a7c684ee99402e95b8a495d
Shlomo Kraus <plonit7@gmail.com>
2018-09-24 13:17:46 +0300

    Remove dist and lib folders

--- dist/Parser.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Parser {
-    static parse(src: string, filename: string, prettier?: boolean): any;
-    static prepare(src: string, options: any): any;
-    static diff(src: any, target: any, ignoreKeys?: string[]): any;
-    static flatten(obj: any): any;
-}
--- lib/Parser.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Parser {
-    static parse(src: string, filename: string, prettier?: boolean): any;
-    static prepare(src: string, options: any): any;
-    static diff(src: any, target: any, ignoreKeys?: string[]): any;
-}
